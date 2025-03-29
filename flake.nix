ed9c17dd76048191bbfdad12032a4a1c5e05d154
siggnal <siggnal@proton.me>
2025-03-29 15:00:24 -0600

    Removed flake-utils from flake build process

--- flake.nix
+++ flake.nix
@@ -1,33 +1,27 @@
 {
   description = "PSXify, a simple script to turn any image file into a PS1 texture.";
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
 
-  inputs = {
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+  outputs = { self, nixpkgs, poetry2nix }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        inputsFrom = [ self.packages.${system}.default ];
+				nativeBuildInputs = with pkgs; [
+				  python312Packages.black
+				  python312Packages.mypy
+				  python312Packages.flake8
+				];
+      };
+
+      packages.${system}.default = mkPoetryApplication {
+        projectDir = self;
+      };
     };
-  };
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-
-      in {
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.default ];
-					nativeBuildInputs = with pkgs; [
-					  python312Packages.black
-					  python312Packages.mypy
-					  python312Packages.flake8
-					];
-        };
-
-        packages.x86_64-linux.default = mkPoetryApplication {
-          projectDir = self;
-        };
-      }
-    );
 }
