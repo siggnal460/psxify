19528ad9b9700f416896fd13cf7efa7793698432
siggnal <siggnal@proton.me>
2025-03-29 17:51:25 -0600

    Fixed dev shell

--- flake.nix
+++ flake.nix
@@ -6,25 +6,26 @@
 
   outputs = { self, nixpkgs, poetry2nix }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      currentSystem = "x86_64-linux";
-			pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
-      devShells.${currentSystem}.default = pkgs.mkShell {
-        inputsFrom = [ self.packages.${currentSystem}.default ];
-				nativeBuildInputs = with pkgs; [
-				  python312Packages.black
-				  python312Packages.mypy
-				  python312Packages.flake8
-				];
-      };
-
       packages = forAllSystems (system: let
         inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
       in {
         default = mkPoetryApplication { projectDir = self; };
+      });
+
+      devShells = forAllSystems (system: let
+        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv;
+      in {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system}; [
+            (mkPoetryEnv { projectDir = self; })
+						poetry
+          ];
+        };
       });
     };
 }
